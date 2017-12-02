6e1dbbe00ae170eff8ca50bb5d07344b92674d0c
xiangcheng <liuxm20@lenovo.com>
2017-12-02 13:53:35 +0800

    添加滑动的activity

---
+++ app/src/main/java/com/single/flowlayout/ViewPagerActivity.java
@@ -0,0 +1,10 @@
+package com.single.flowlayout;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xiangcheng on 17/12/2.
+ */
+
+public class ViewPagerActivity extends AppCompatActivity {
+}
